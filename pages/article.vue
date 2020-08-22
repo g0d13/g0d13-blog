d0333510b79b43fc5c7a637843da85d642659ac9
Juan Diego Parra Mendez <g0d13@outlook.com>
2020-08-22 00:14:44 -0500

    page article

---
+++ pages/article.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container mx-auto">
+    <article
+      class="prose-sm prose md:mx-10 lg:mx-20 xl:mx-40 sm:prose max-w-none"
+    >
+      <h1>{{ page.title }}</h1>
+      <nuxt-content :document="page"></nuxt-content>
+    </article>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content }) {
+    const page = await $content('hello').fetch()
+    return {
+      page,
+    }
+  },
+}
+</script>
+
+<style>
+.prose * {
+  color: var(--color) !important;
+}
+
+.prose code {
+  font-weight: 600;
+  font-size: 0.875em;
+}
+
+.max-w-none {
+  max-width: none;
+}
+</style>
