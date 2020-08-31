5b89057bb073c33a45d6528c1c92154c26d5f0b4
Juan Diego Parra Mendez <g0d13@outlook.com>
2020-08-31 10:15:47 -0500

    created about

---
+++ pages/about.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    Iconos diseñados por
+    <a
+      href="https://www.flaticon.es/icono-gratis/codificacion_759823"
+      title="Kiranshastry"
+      >Kiranshastry</a
+    >
+    from <a href="https://www.flaticon.es/" title="Flaticon">www.flaticon.es</a>
+  </div>
+</template>
